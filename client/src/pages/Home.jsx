232a3c214e232b66ee9e5e92bf246a29235cafcf
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-14 19:10:06 +0100

    fetching posts from cloudinary

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,6 +18,32 @@ function Home() {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/post", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
